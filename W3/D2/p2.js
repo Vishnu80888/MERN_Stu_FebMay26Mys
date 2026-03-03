9024da7319fd56d876df0670f591a887a49c7188
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-03-03 15:33:48 +0530

    comparison operators

---
+++ W3/D2/p2.js
@@ -0,0 +1,13 @@
+//comparison operators
+
+console.log("Comparison operators");
+const x = 2 , y = 3;
+console.log("x==y: ",x==y);//loose equlity
+console.log("x===y: ",x===y);//strict
+console.log("x!=y: ",x!=y);
+console.log("x!==y: ",x!==y);
+console.log("x>3: ",x>3);
+console.log("x<3: ",x<3);
+console.log("x>=3: ",x>=3);
+console.log("x<=3: ",x<=3);
+
