9fa5d8871b3a14fc7a71626153322d2fd10f40e5
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-03-04 14:35:53 +0530

    JSON Stringify and Parse

---
+++ W3/D3/p8.js
@@ -0,0 +1,13 @@
+// JSON Stringify and Parse
+const employee = {
+    id : 102,
+    name:"Sandeep",
+    dept : "ME",
+    isActive : true,
+};
+const jsonString = JSON.stringify(employee);
+console.log(jsonString)
+
+//parseing
+const parsedObject = JSON.parse(jsonString);
+console.log(parsedObject)
