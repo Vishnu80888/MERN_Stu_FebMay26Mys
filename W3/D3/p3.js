2a2b76dc1cf7e29af634d4a308e90ae78dbd7de3
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-03-04 13:27:19 +0530

    Nested loops

---
+++ W3/D3/p3.js
@@ -0,0 +1,14 @@
+// Nested loops 
+for( let i=0;i<5;i++){
+    if(i===2){
+        console.log("i",i);
+        break;
+    }
+}
+
+for( let i=0;i<5;i++){
+    if(i===2){
+        console.log("i",i);
+        continue;
+    }
+}
