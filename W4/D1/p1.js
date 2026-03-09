8022532bfbb11a7cab7e6b2dc533075d5c30da60
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-03-09 17:40:50 +0530

    try catch basics

---
+++ W4/D1/p1.js
@@ -0,0 +1,31 @@
+// //try catch basics
+// const error = document.getElementById("error");
+
+// try{
+//     console.log("Trying to access undefined variable");
+//     console.log(notDefined);
+
+
+// }
+// catch(err){
+//     console.log("Error caught",err.name,"-",err.message);
+// }
+// console.log("Program execution continues");
+
+//JSOn parsing error
+// let jsonText = "{json}";
+// try{
+//     let data = JSON.parse(jsonText);
+//     console.log(data);
+// }
+// catch(err){
+//     console.log("JSON pare error",err.message);
+// }
+
+try{
+    let num = 10;
+    num();
+}
+catch(err){
+    console.log("JSON Caught error:",err.name);
+}
