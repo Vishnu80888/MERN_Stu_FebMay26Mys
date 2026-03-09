737d11d2d479dab2eb2f4637a84e3fb77806609c
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-03-09 17:35:43 +0530

    Event Handling: basic of click Events

---
+++ W3/D5/p1.js
@@ -0,0 +1,10 @@
+//basic click events
+const button = document.getElementById("clickbtn");
+
+button.addEventListener("click",function() {
+    console.log("button is clicked");
+});
+
+button.addEventListener("dblclick",function() {
+    console.log("second button is clicked");
+});
