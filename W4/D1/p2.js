4ece5301bbab9a8ffddaabfa56ae99ddd4fcf573
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-03-09 17:41:11 +0530

    try catch and finally block

---
+++ W4/D1/p2.js
@@ -0,0 +1,16 @@
+//try catch and finally block
+function parseConfig(text){
+    try{
+        let config = JSON.parse(text);
+        return config;
+    }
+    catch(err){
+        console.log("Error parsing config",err.message);
+        return null;
+    }
+    finally{
+        console.log("Finally block executed");
+    }
+}
+parseConfig('{"theme":"dark"}');
+parseConfig("{darshan}");
