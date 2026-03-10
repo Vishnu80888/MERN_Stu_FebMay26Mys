1e66ab0679bfa75e00edab5ba0116b9f375e269f
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-03-10 11:17:57 +0530

    CreateElement() & appendChild()

--- W4/D2/p2.js
+++ W4/D2/p2.js
@@ -1,20 +1,21 @@
-//creatElements() && appendChild()
+// CreateElement() & appendChild()
 
 const list = document.getElementById("list");
 let counter = 1;
-
-document.getElementById("addbtn").addEventListener("click",function(){
-    list.textContent ="";
+document.getElementById("addBtn").addEventListener("click",
+    function(){
+        errMessage.textContent =" ";
     const li = document.createElement("li");
-    li.textContent = "Item " + counter++;
+    li.textContent = "Item "+ counter++;
     list.appendChild(li);
-});
-
-document.getElementById("removebtn").addEventListener("click",function(){
-    if(list.lastElementChild){
-    list.removeChild(list.lastElementChild);
     }
-    else{
-        list.textContent ="no items to remove";
-    }
-});
+);
+document.getElementById("rmBtn").addEventListener("click",
+    function(){
+        if (list.lastElementChild) {
+        list.removeChild(list.lastElementChild);
+        }
+        else{
+             errMessage.textContent ="No items to delete";
+        }
+    });
