7d373e2cc4e7c2edb1d2c8fb8405b1a47d929788
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-03-17 16:18:20 +0530

    Chaining promise with returned promises

---
+++ W5/D2/p11.js
@@ -0,0 +1,30 @@
+//Chaining promise with returned promises
+function getOrderId(){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve(501);
+        },500);
+    });
+}
+function getOrderDetails(orderId){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve({
+                id:orderId,
+                product:"Laptop",
+                quantity :2
+            });
+        },700);
+    });
+}
+
+getOrderId()
+.then(function(orderId){
+    console.log("Order Id received:",orderId);
+    return getOrderDetails(orderId);
+})
+.then(function(orderDetails){
+    console.log("Order details loaded");
+    console.log("product:",orderDetails.product);
+    console.log("Quantity:",orderDetails.quantity);
+})
