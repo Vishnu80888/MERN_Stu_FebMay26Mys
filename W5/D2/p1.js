810a2c10370e5f8f32ae5a3339377fbe1aaebefd
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-03-17 13:28:58 +0530

    Introduction to callback Function in node.js

---
+++ W5/D2/p1.js
@@ -0,0 +1,10 @@
+//Introduction to callback Function in node.js
+function greetUser(name,callback){
+    console.log("Hello,"+name);
+    //The callback function is executed only after the execution of the current function
+    callback();
+}
+function showCompletionMessage(){
+    console.log("The greeting task is complete.");
+}
+greetUser("Vishnu",showCompletionMessage);
