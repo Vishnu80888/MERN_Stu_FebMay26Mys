0bb9db9f88b71d9ad8fee5aaa548a9eccb0f551c
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-03-17 13:31:15 +0530

    Callback Nesting

---
+++ W5/D2/p6.js
@@ -0,0 +1,19 @@
+//Callback Nesting
+console.log("Starting nested callback flow");
+
+setTimeout(function(){
+    console.log("Step 1: user loaded.");
+
+    setTimeout(function(){
+        console.log("Step 2: Orders loaded.");
+
+        setTimeout(function(){
+            console.log("Step 3: Payments loaded..");
+
+            setTimeout(function(){
+                console.log("Step 4: Shipment loaded.");
+                console.log("We are in Callback hell!!..");
+            },1000);
+        },800);
+    },600);
+},400);
