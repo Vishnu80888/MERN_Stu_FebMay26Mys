779b06ffc06ce3d32033c5ed84b2641ef642c478
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-03-26 17:28:40 +0530

    Understanding the HTTP module

---
+++ W6/D4/p1.js
@@ -0,0 +1,7 @@
+// Understanding the HTTP module
+//Importing bulit-in http module...........
+
+const http = require("http");
+
+console.log("Type of http.createSever:",typeof http.createServer);
+console.log("Common HTTP methods:",http.METHODS);
