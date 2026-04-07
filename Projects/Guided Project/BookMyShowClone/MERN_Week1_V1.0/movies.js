732e5a0fd0d99b53472dc0a3623c4d7d4c356978
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-04-07 09:34:24 +0530

    updated code

--- Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/movies.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/movies.js
@@ -5,27 +5,27 @@ const movies = [
         id : 1,
         title: "Dhurandar 2",
         showtimes: [
-            {time:"10:00AM",seatsAvailable:100},
-            {time:"01:00PM",seatsAvailable:80},
-            {time:"06:00PM",seatsAvailable:300},
+            {time:"10:00AM",seatsAvailable:10},
+            {time:"01:00PM",seatsAvailable:8},
+            {time:"06:00PM",seatsAvailable:30},
         ]
     },
      {
         id : 2,
         title: "LoveMockTail 3",
         showtimes: [
-            {time:"09:00AM",seatsAvailable:100},
-            {time:"01:00PM",seatsAvailable:80},
-            {time:"07:00PM",seatsAvailable:300},
+            {time:"09:00AM",seatsAvailable:10},
+            {time:"01:00PM",seatsAvailable:8},
+            {time:"07:00PM",seatsAvailable:30},
         ]
     },
      {
         id : 3,
         title: "Hayagreeva",
         showtimes: [
-            {time:"11:00AM",seatsAvailable:100},
-            {time:"02:00PM",seatsAvailable:80},
-            {time:"07:00PM",seatsAvailable:300},
+            {time:"11:00AM",seatsAvailable:10},
+            {time:"02:00PM",seatsAvailable:8},
+            {time:"07:00PM",seatsAvailable:30},
         ]
     }
 ];
