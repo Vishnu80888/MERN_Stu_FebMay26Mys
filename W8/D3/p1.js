d75c4e392c5c0f5887af1ef56d6712bc624067eb
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-04-14 11:42:24 +0530

    Validation and scehma constraints

---
+++ W8/D3/p1.js
@@ -0,0 +1,37 @@
+//Validation and scehma constraints
+const mongoose = require ("mongoose");
+
+const userSchema =  new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        
+    },
+    age:{type:Number,min:18},
+    role:{type:String,enum:["admin","user","manager"]
+    },
+    email:{type:String,match:/.+@.+\..+/}
+});
+const User = mongoose.model("HookValidationUser",userSchema);
+
+async function runValidationDemo(){
+    try{
+        const invalidUser = new User({
+            name:vishnu,
+            age:20,
+            role:"user",
+            email:"user@gmail.com"
+        });
+
+        await invalidUser.validate();
+    }
+    catch(error){
+        console.log("Validation errors found:");
+        for(const fieldName in error.errors){
+            console.log(fieldName+":"+error.errors[fieldName].message);
+        }
+    }
+}
+runValidationDemo();
+
+
