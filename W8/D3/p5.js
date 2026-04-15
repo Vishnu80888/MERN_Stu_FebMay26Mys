77793ed2b82a2758987f78d13c20caba8837a6c7
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-04-15 12:13:20 +0530

    Relationship patterns & Types

---
+++ W8/D3/p5.js
@@ -0,0 +1,124 @@
+// Relationship patterns
+const mongoose = require("mongoose");
+
+async function main() {
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/embrel");
+        console.log("Connected to MongoDB");
+
+    
+        // 1. ONE-TO-MANY (EMBEDDING)
+        
+        const blogSchema = new mongoose.Schema({
+            title: String,
+            comments: [
+                {
+                    text: String
+                }
+            ]
+        });
+
+        const Blog = mongoose.model("Blog", blogSchema);
+
+        await Blog.deleteMany();
+
+        const blog = await Blog.create({
+            title: "Mongoose Basics",
+            comments: [   // ✅ fixed spelling
+                { text: "Great article" },
+                { text: "Helpful article" }
+            ]
+        });
+
+        console.log("Embedding:");
+        console.log(await Blog.find());
+
+        // 2. ONE-TO-MANY (REFERENCING)
+        
+        const postSchema = new mongoose.Schema({
+            title: String
+        });
+
+        const commentSchema = new mongoose.Schema({
+            text: String,   
+            post: {
+                type: mongoose.Schema.ObjectId,
+                ref: "Post"
+            }
+        });
+
+        const Post = mongoose.model("Post", postSchema);
+        const Comment = mongoose.model("Comment", commentSchema); 
+
+        await Post.deleteMany();
+        await Comment.deleteMany(); 
+
+        const post = await Post.create({ title: "NodeJS Basics" });
+
+        await Comment.create([
+            { text: "Nice post", post: post._id },
+            { text: "Good", post: post._id }
+        ]);
+
+        // console.log("Referencing:");
+        // console.log(await Comment.find().populate("post"));
+
+        //Many to many
+        const studentSchema= new mongoose.Schema({
+            name:String,
+            courses:[{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:'Course'
+            }
+            ]
+        });
+        const courseSchema = new mongoose.Schema({
+            title:String,
+            students:[
+                {
+                    type:mongoose.Schema.Types.ObjectId,
+                    ref:'student'
+                }
+            ]
+        });
+
+        const Student = mongoose.model('Student',studentSchema);
+        const Course = mongoose.model('Course',courseSchema);
+
+        const course1 = await Course.create({title:"MongoDB"});
+        const course2 = await Course.create({title:"Node JS"});
+        const course3 = await Course.create({title:"Express JS"});
+
+        const student1 = await Student.create({
+            name:"vishnu",
+            courses:[course1._id,course2._id]
+        });
+        const student2 = await Student.create({
+            name:"Darshan",
+            courses:[course2._id,course3._id]
+        });
+        const student3 = await Student.create({
+            name:"vishnu",
+            courses:[course1._id]
+        });
+        course1.students.push(student1._id,student2._id);
+        course2.students.push(student2._id,student3._id);
+        course3.students.push(student1._id);
+
+        await course1.save();
+        await course2.save();
+        await course3.save();
+
+        console.log("Many-to-Many:");
+        console.log(await Student.find().populate('courses'));
+        console.log(await Course.find().populate('students'));
+        
+    } catch (err) {
+        console.log("Error:", err.message);
+    } finally {
+        await mongoose.disconnect();
+        console.log("DB disconnected");
+    }
+}
+
+main();
