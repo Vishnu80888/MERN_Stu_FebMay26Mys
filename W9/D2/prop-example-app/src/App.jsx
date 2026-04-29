eb45ead723b9286376b0268795ac82189db77b39
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-04-29 10:22:35 +0530

    prop drilling

---
+++ W9/D2/prop-example-app/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css'
+// import { WelcomeCard } from "./components/p1";
+// import { PropDestructuring } from "./components/p2";
+// import { PropsChildren } from "./components/p3";
+import { PropDrillingDemo } from "./components/p4";
+
+function App() {
+  return (
+    <div className="container">
+      {/* <WelcomeCard/>  */}
+      {/* <PropDestructuring/>
+      <PropsChildren/> */}
+      <PropDrillingDemo/>
+    </div>
+  );
+}
+
+export default App
