fe0b95e26d0aa73bdfb036e5728e1045473ba9e8
Vishnu80888 <vishnumahendarreddy@gmail.com>
2026-04-29 14:23:03 +0530

    Passing Function as Props

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -1,31 +1,23 @@
 import React from "react";
-import './App.css'
+import "./App.css";
 // import { WelcomeCard } from "./components/p1";
 // import { PropDestructuring } from "./components/p2";
 // import { PropsChildren } from "./components/p3";
 // import { PropDrillingDemo } from "./components/p4";
 // import { SharedStateParent } from "./components/p5";
+// import { CompositionDemo } from "./components/p6";
+// import { ObjectsAndArrayProps } from "./components/p7";
+// import { PropsImmutability } from "./components/p8";
+import { FunctionProps } from "./components/p10";
 
-//  import { CompositionDemo } from "./components/p6";
-  // import { ObjectsAndArrayProps } from "./components/p7";
-  // import { PropsImmutability } from "./components/p8";
-  import { PropsState } from "./components/p9";
-
+import PropsState from "./components/p9"; // ✅ fixed
 
 function App() {
   return (
     <div className="container">
-      {/* <WelcomeCard/>  */}
-      {/* <PropDestructuring/>
-      <PropsChildren/> */}
-      {/* <PropDrillingDemo/> */}
-      {/* <SharedStateParent/> */}
-      {/* <CompositionDemo/> */}
-      {/* <ObjectsAndArrayProps/> */}
-      {/* <PropsImmutability/> */}
-      <PropsState/>
+      <FunctionProps/>
     </div>
   );
 }
 
-export default App
+export default App;
